da8fc8eb0a9f1adbf34f120c18ec3e675b4a9397
rontap <aron.tatai@gmail.com>
2015-06-10 21:25:16 +0200

    4.3.1

---
+++ api/note.js
@@ -0,0 +1,170 @@
+//NOTE.JS created by rontap 2015
+// cc-by-sa
+
+Array.prototype.max = function() {
+    return Math.max.apply(null, this)
+    };
+
+    Array.prototype.min = function() {
+      return Math.min.apply(null, this)
+    };
+    
+    appId="10";
+    
+    notes=[];
+    //for (i=0;i<100;i++) { notes[i] = {}; };
+    note = {
+        length:0,
+        
+    objects:[],
+    elements : [ 'name' , 'content' ,'created' ,'modified' ,'status'],
+    parse : function( id ) { return JSON.parse(localStorage['note'+appId+'_'+id]) ;}  ,  
+    //stringify : function( id ) { return JSON.stringify(localStorage['note'+appId+'_'+id]) ;}  ,  
+    current:0,
+    sel : function(what) {//important preprocessor
+        var what;    
+        
+        if (!isNaN(Number(what)) ){return note.getById(what); console.log('hm');}
+        else if  (what=="") {return notes[note.current];}
+        else {return what;}
+    },
+    new : function(name,content)
+    {
+       notes[notes.length]= {
+           id:notes.length,
+           name:name,
+           content:content,
+           created: new Date(),
+           modified: '0',
+           status : 21
+       }
+       note.length++;
+    } ,
+    getById : function(id) {
+        return notes[id];
+    } , 
+    edit : function(what,toWhat) {
+        what = note.sel(what);
+        notes[what.id].name = toWhat.name;
+        notes[what.id].content = toWhat.content;
+        notes[what.id].modified = new Date();
+        notes[what.id].status = 20 ;
+        },
+    status :  function(status,what,life) {
+        if (typeof(life)==='undefined') life = false;      
+        console.log(what);
+        //te=note.sel(what);
+        te=   notes[note.current];
+
+         console.log(te);
+         if (life) { 
+             prevstat= notes[te.id].status ;
+             setTimeout(function(){
+                notes[te.id].status=prevstat  ;
+                note.load(te); 
+             },250)      
+         }     
+        notes[te.id].status = status; 
+        note.load(te); 
+    },
+    save : function(what) {
+		
+	},
+    load : function(what)  {
+        what=note.sel(what);
+	//	console.log(what);
+        note.current=what.id;
+           
+           for (i=0;i<note.elements.length;i++) {
+               note.loadhelp(note.elements[i], what);
+               //console.log(note.elements[i],what);
+           }
+        },
+    loadhelp : function(query,what) { 
+		//console.log(what);
+        document.getElementsByClassName('note-object')[0].querySelector('[is="'+query+'"]').value = what[query]; 
+    },
+    push: function(what) {
+        note.status(35,'',true);
+        localStorage['note'+appId+'_'+what.id]= JSON.stringify(what);
+        },
+    pull: function(id,what) { //we need ID
+        note.status(30,'',true);
+        note.edit(note.current, note.parse(id));
+        ext.updated();
+        note.load(what, note.elements);
+        return notes[id]
+        },
+    sync: function (what) {
+        if (what.modified <= note.parse(what.id).modified ) {note.push(what); } 
+        else { note.pull(what.id,what); alert("syncing down");}
+        ext.updated();
+    },
+    stat : {
+            cc        : function(what) {   return what.content.length;  },
+            ccNoSpace : function(what) {   return what.content.length-what.content.split(" ").length;  },
+            wc        : function(what) {   return what.content.split(/\s+/).length;  },
+            letters   : function(what) {    //sort the letters
+                a=what.content.toLowerCase();
+                letterStore=[];//where we store the actual letters
+                letterCount=[];//wwhere we store those
+                for (i=0;i<a.length;i++) {                    
+                    index=letterStore.indexOf(a[i]);//to save power, we calculate this here
+                    if (index<0) { //if not
+                        letterStore[letterStore.length]=a[i]; 
+                        letterCount[letterCount.length]=1;
+                    }
+                    else {
+                        if (letterCount[index]===undefined)     letterCount[index]=0;
+                        else                                    letterCount[index]++;
+                    }
+                }
+                return [letterCount,letterStore];
+                
+            },
+            letterMax  : function(what) { 
+                text=note.stat.letters(what);
+                return text[1][ text[0].indexOf( text[0].max() ) ] 
+                //     get the most freq. letter from the index of the most used letter
+            }, 
+            letterMin  : function(what) { 
+                text=note.stat.letters(what);
+                return text[1][ text[0].indexOf( text[0].min() ) ] 
+                //     get the most freq. letter from the index of the most used letter
+            },
+            letterSort : function(what) { 
+                text=note.stat.letters(what);
+                output=[[],[]];
+                for (i=0;i<text[0].length;i++)
+                    {
+                         output[0][output[0].length] = text[0][ text[0].indexOf( text[0].max() ) ]
+                         output[1][output[1].length] = text[1][ text[0].indexOf( text[0].max() ) ]
+                         text[0][ text[0].indexOf( text[0].max() ) ] = 0 ;
+                    }
+               return output;
+            },
+            ultrastat : function(what) {
+                ab=[];
+                ab[ab.length] = note.stat.cc(what);
+                ab[ab.length] = note.stat.ccNoSpace(what);
+                ab[ab.length] = note.stat.wc(what);
+                ab[ab.length] = note.stat.letterMax(what);
+                ab[ab.length] = note.stat.letterMin(what);
+                ab[ab.length] = note.stat.letterSort(what);
+                return ab ;
+            }
+        
+            
+        }//stat
+    
+    } 
+    if (localStorage['note'+appId+'_length']==undefined) { localStorage['note'+appId+'_length']='0'; }
+    else note.length=localStorage['note'+appId+'_length'];
+    
+    ext = {
+        updated : function() {
+             modis.value=note.getById(0).modified;
+			creatis.value=note.getById(0).created;
+			statusis.value=note.getById(0).status;
+        }
+    }
--- tools/wiki.php
+++ tools/wiki.php
@@ -1,9 +1,11 @@
+<head>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700|Noto+Serif:400,700&subset=latin,cyrillic,greek,latin-ext' rel='stylesheet' type='text/css'>
+
 <link rel="import" href="../../polyfome/include_minimalist.html">
 <link rel="import" href="wikidata/header.html">
 
-
+</head>
 <script>
 
 console.log('hello');
@@ -23,6 +25,9 @@ function removeStyles(el) {
 
 
 </script>
+
+
+
 <container id="ULTRAC">
     <?php
     if (isset( $_GET['q']))
@@ -34,17 +39,28 @@ function removeStyles(el) {
     }
     ?>
 </container>
+<div id="ULTILOAD_WRAP">&nbsp;</div>
+<img src="https://thomas.vanhoutte.be/miniblog/wp-content/uploads/spinningwheel.gif" id="ULTILOAD">
+
+<style>
+    body:not(.XRT-LOADED) #firstHeading {z-index:999; position:fixed; top:50px; left:0px; right:0px; text-align:center; border:none;}
+    #ULTILOAD_WRAP, #ULTILOAD { transition:.4s;}
+    #ULTILOAD_WRAP { position:fixed; top:0; left:0; right:0; bottom:0; background:#ededee;}
+#ULTILOAD {position:fixed;top:0; left:calc(50% - 161px); top:calc(50% - 111px);}
+    .XRT-LOADED #ULTILOAD,.XRT-LOADED #ULTILOAD_WRAP {opacity:0; !important; z-index:-2;}
+</style>
+
 <script>
 setTimeout(function() {
 	console.clear();
-	removeStyles(document.body);
-	$('link[rel="stylesheet"]').remove();
+	removeStyles(ULTRAC);
+	$('#ULTRAC link[rel="stylesheet"]').remove();
 	$('body').prepend('<mw-nav-header-element></mw-nav-header-element>');
 	$('body').prepend('<mw-bottom-navigator></mw-bottom-navigator>');
-	
+	 
 	//definite
 	$('#mw-head-base').append('<div class="toc_nav"><paper-icon-button icon="view-day"></paper-icon-button><span>Tartalomjegyzék</span></div>');
-    $('#p-lang').append('<div class="plang_nav"><paper-icon-button icon="language"></paper-icon-button><span>Nyelv</span></div>');
+    $('#mw-head-base').append('<div class="plang_nav"><paper-icon-button icon="language"></paper-icon-button><span>Nyelv</span></div>');
      
         $('.ujinfobox').addClass('card away maximise');
     
@@ -53,21 +69,61 @@ setTimeout(function() {
     $('.toc_nav').click(function(){
 	   $('body').toggleClass('toctoggle_open');
     })
-    $('#plang_nav').click(function(){
+    $('.plang_nav').click(function(){
 	   $('body').toggleClass('plangnav_open');
     })
     $('#mw-fr-revisiontag').html('');
     $('.mw-editsection').html('');
     
    
-
+    $('img[alt="Searchtool right.svg"]').each(function() {
+       setImageToSVG(this,more_info_bar);
+    });
+     $('img[alt="Wiki letter w.svg"]').each(function() {
+       setImageToSVG(this,expand_article_bar);
+    });
+     $('img[alt="Ambox important.svg"]').each(function() {
+       setImageToSVG(this, warning_article_bar);
+    });
+   
+    $('.navbox').addClass('card away');
+    $('body').addClass('XRT-LOADED');
 },2500);
 
-
+function setImageToSVG(img,svg){
+  var xml = (new XMLSerializer).serializeToString(svg);
+  img.src = "data:image/svg+xml;charset=utf-8,"+xml;
+}
 </script>
+
+
+<div style="display:none;" id="new_age_images">
+<svg id="more_info_bar" viewBox="0 0 24 24" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" fit="" style="pointer-events: none; display: block;"><g><path d="M22 3h-15c-.69 0-1.23.35-1.59.88l-5.41 8.12 5.41 8.11c.36.53.97.89 1.66.89h14.93c1.1 0 2-.9 2-2v-14c0-1.1-.9-2-2-2zm-13 10.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm5 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm5 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" fill="#48F"></path></g></svg>
+
+<svg id="expand_article_bar" viewBox="0 0 24 24" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" fit="" style="pointer-events: none; display: block;"><g><path d="M5 17.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zm4-4.5c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm0-4c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm-6 12h18v-2h-18v2zm2-11.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zm0 4c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zm4 3.5c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm8-.5c.28 0 .5-.22.5-.5s-.22-.5-.5-.5-.5.22-.5.5.22.5.5.5zm-14-13.5v2h18v-2h-18zm14 5.5c.28 0 .5-.22.5-.5s-.22-.5-.5-.5-.5.22-.5.5.22.5.5.5zm0 4c.28 0 .5-.22.5-.5s-.22-.5-.5-.5-.5.22-.5.5.22.5.5.5zm-4-3.5c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm0 4c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm0 4c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1z" fill="#f44"></path></g></svg>    
+   
+    
+    <svg id="warning_article_bar" viewBox="0 0 24 24" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" fit="" style="pointer-events: none; display: block;"><g><path d="M1 21h22l-11-19-11 19zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" fill="#f44"></path></g></svg>
+</div>
+
+
 
 <style>
     
+   #more_info_bar { fill:#48F;}
+    img[alt="Searchtool right.svg"] { width:20px; height:20px;}
+   #new_age_images {
+       display:none;
+       
+   }
+   .wb-langlinks-edit a {  display:none;}
+    .navbox td:not(:empty) {
+        border: 1px solid #999;
+    }
+    .navbox { font-family: RobotoDraft, sans-serif;}
+     .navbox td:empty { display:none;}
+    .navbox th span:first-child { display:none;}
+    .navbox th span:last-child { font-size:25px; margin:10px;}
 .toc, #p-lang{
 	position:fixed;
 	top:50px;
@@ -77,9 +133,13 @@ setTimeout(function() {
 	width:250px;
     left:-250px;
     background:rgb(238, 238, 238);
+    
+    background: white;
+  margin-top: 0px !important;
+  margin-bottom: 26px !important;
  
 }
-    .toctoggle_open .toc, .plangnav_open {
+    .toctoggle_open .toc ,.plangnav_open #p-lang {
         left:0px;
     }
     .toc li a {
@@ -105,25 +165,27 @@ setTimeout(function() {
         fill:#22B94F;
     }
         /*acitve*/
-        .toc_nav, #p-lang .plang_nav {
+        .toc_nav, .plang_nav {
             position:fixed;
             left:20px;
             top:50px;        
         }
-        #p-lang .plang_nav {
+         .plang_nav {
             top:90px;
         }
         .toc_nav paper-icon-button,.plang_nav paper-icon-button {
             vertical-align:-13px;
         }
 
-        .toctoggle_open  .toc_nav {
-            left:270px;
+        .toctoggle_open  .toc_nav , .plangnav_open .plang_nav{
+            left:270px !important;
         }
-        .toctoggle_open  .toc_nav span{
+    .toctoggle_open  .plang_nav {opacity:0;}
+    .plangnav_open .toc_nav {opacity:0;}
+        .toctoggle_open  .toc_nav span, .plangnav_open .plang_nav span{
             opacity:0;
         }
-        .toctoggle_open .mw-body {
+        .toctoggle_open .mw-body , .plangnav_open .mw-body {
           padding-left:250px;
         } 
         .mw-body {
@@ -137,7 +199,7 @@ setTimeout(function() {
 display:none !important;
 opacity:0;
 }
- #toctitle ,.toc,.mw-body,.toc_nav  {
+ #toctitle ,.toc,.mw-body,.toc_nav, #p-lang, .plang_nav  {/*expand navigation bar transition*/
  transition:all .4s;
     }
 body {
@@ -156,10 +218,13 @@ font-family: 'PT Sans', sans-serif;
     .maximise {
         width:800px;
     }
-    .focus_links a {
+    .focus_links a { /*focus linux*/
         transition:color .4s;
         text-decoration:none;
         color:#222;
+    }
+    .focus_links .reference {
+        opacity:0;
     }
     img.panorama {
        position:absolute;
